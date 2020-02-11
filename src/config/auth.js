99337f83304a8b770399e958d65d17ba4aad1742
belapferreira <isabelapenhaferreira@gmail.com>
2020-02-11 12:48:20 -0300

    chore: Commit inicial

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9aba9185f7b462f51911ff5ca9d539d1', // gobarberlearningbootcampgostack2020
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gobarber',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
